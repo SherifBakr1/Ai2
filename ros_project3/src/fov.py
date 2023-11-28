88df5946b05501b4b8f17dcd4ae5e3905930d2ca
Sherif Bakr <96317047+SherifBakr1@users.noreply.github.com>
2023-11-28 16:42:51 -0700

    fov test

--- ros_project3/src/fov.py
+++ ros_project3/src/fov.py
@@ -13,18 +13,18 @@ def laserscan_callback(msg):
     # Print or process the right 5 degrees lidar data
     #print("Right", msg.ranges[270:271])
     ##print("Front", msg.ranges[0:1])
-    print("Back: ", msg.ranges[180:181] )
+    print("Back: ", min(msg.ranges[180:181]) )
     #if min(msg.ranges[330:331]) > 2:
     print("Laila 3elwy")
 
 
-    range_right = msg.ranges[255:270]  # right FOV (between 300 to 345 degrees)
-    min_right,i_right = min( (range_right[i_right],i_right) for i_right in range(len(range_right)) )
+    range_right = min(msg.ranges[255:270])  # right FOV (between 300 to 345 degrees)
+    #min_right,i_right = min( (range_right[i_right],i_right) for i_right in range(len(range_right)) )
 
     print("min right: ", min_right)
 
-    range_front =msg.ranges[0:1]
-    min_front,i_front = min( (range_front[i_front],i_front) for i_front in range(len(range_front)) )
+    range_front =min(msg.ranges[0:1])
+    #min_front,i_front = min( (range_front[i_front],i_front) for i_front in range(len(range_front)) )
 
     print("min front: ", min_front)
 
